12c88ee2101636b567483848fd520d19f4ef6cc3
Liza Mohamad Ali Aziz <lizamaa@kth.se>
2026-04-27 10:48:14 +0200

    add: the log-transformed on the data before cleaning to stabilize variance

--- main/data_cleaning.R
+++ main/data_cleaning.R
@@ -7,34 +7,32 @@ file_path = "dataset/se_d_data.csv"
 data_orig <- read.csv(file_path)
 data_orig$date <- as.Date(data_orig$date, format = "%Y-%m-%d")
 
-# Edited (cleaned) data
-# copy data
-data_clean <- data_orig 
-# remove last data point since outlier and drops to zero (incomplete data point)
-data_clean <- subset(data_clean, date < as.Date("2026-04-12"))
-# convert to time series object
-ts_clean <- ts(data_clean$SE3)
-# tsclean() removes any other hidden outlier
-ts_clean_val <- tsclean(ts_clean)
+# Log-transformed data
+# copy data and remove extreme outlier and aply log-transform
+data_log <- subset(data_orig, date < as.Date("2026-04-12"))
+data_log$SE3_log <- log(data_log$SE3)
+# create ts object
+ts_log <- ts(data_log$SE3_log, frequency = 7)
+ts_clean_val <- tsclean(ts_log)
 
 df_clean <- data.frame(
-  date = data_clean$date,
+  date = data_log$date,
   SE3 = as.numeric(ts_clean_val)
 )
 
 fig_orig <- plot_ly(data_orig, x = ~date, y = ~SE3, type = 'scatter', mode = 'lines', 
                     line = list(color = 'black', width = 1), name = 'Original (Dirty)')
 
-fig_clean <- plot_ly(df_clean, x = ~date, y = ~SE3, type = 'scatter', mode = 'lines', 
-                     line = list(color = 'steelblue', width = 1), name = 'Cleaned')
+fig_log <- plot_ly(data_log, x = ~date, y = ~SE3_log, type = 'scatter', mode = 'lines', 
+                     line = list(color = 'steelblue', width = 1), name = 'Log Transformed')
 
-interactive_plot <- subplot(fig_orig, fig_clean, nrows = 2, shareX = TRUE, titleY = FALSE) %>%
+interactive_plot <- subplot(fig_orig, fig_log, nrows = 2, shareX = TRUE, titleY = FALSE) %>%
   layout(
-    title = 'SE3 Electricity Consumption: Original vs. Cleaned Data',
+    title = 'SE3 Electricity Consumption: Original vs. Log-Transformed',
     height = 700,
     showlegend = TRUE,
     hovermode = "x unified"
   )
 
-saveWidget(interactive_plot, "se3_data_comparison.html", selfcontained = TRUE)
-browseURL("se3_data_comparison.html")
+saveWidget(interactive_plot, "se3_data_comparison_log.html", selfcontained = TRUE)
+browseURL("se3_data_comparison_log.html")
